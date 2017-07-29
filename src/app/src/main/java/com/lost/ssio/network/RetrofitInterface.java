246337fdaa5cac993b7ebac74eca74af4001b50c
Hamza El-Ghoujdami <hamzaelghoujdami@gmail.com>
2017-07-29 16:04:33 +0200

    add src

---
+++ src/app/src/main/java/com/lost/ssio/network/RetrofitInterface.java
@@ -0,0 +1,32 @@
+package com.lost.ssio.network;
+
+import com.lost.ssio.model.Response;
+import com.lost.ssio.model.User;
+
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import rx.Observable;
+
+public interface RetrofitInterface {
+
+    @POST("users")
+    Observable<Response> register(@Body User user);
+
+    @POST("authenticate")
+    Observable<Response> login();
+
+    @GET("users/{email}")
+    Observable<User> getProfile(@Path("email") String email);
+
+    @PUT("users/{email}")
+    Observable<Response> changePassword(@Path("email") String email, @Body User user);
+
+    @POST("users/{email}/password")
+    Observable<Response> resetPasswordInit(@Path("email") String email);
+
+    @POST("users/{email}/password")
+    Observable<Response> resetPasswordFinish(@Path("email") String email, @Body User user);
+}
